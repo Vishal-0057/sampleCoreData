1bdd1d85d7d2a9d6cdee5ca501a6be58c915a987
Vishal Gupta <vishal.gupta@tothenew.com>
2015-07-24 12:27:44 +0530

    Initial Commit

---
+++ sampleCoreData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleCoreData
+//
+//  Created by abhayam rastogi on 7/24/15.
+//  Copyright (c) 2015 Intelligrape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
